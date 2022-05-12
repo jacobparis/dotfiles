25ee5907b13b4c05cc499413872babe29fb7b57a
Jacob Paris <jacob@jacobparis.com>
2022-05-12 01:13:50 +0100

    Update .install.sh

--- .install.sh
+++ .install.sh
@@ -2,8 +2,8 @@
 
 set -eu;
 
-if test ! -e /ide/bin/gp-code || test ! -v GITPOD_REPO_ROOT; then {
-    printf 'error: This script is meant to be run on Gitpod, quiting...\n' && exit 1;
+if test ! -v GITPOD_REPO_ROOT; then {
+    printf 'error: This script is meant to be run on Gitpod, quitting...\n' && exit 1;
 } fi
 
 SOURCE_DIR="$(readlink -f "$0")" && SOURCE_DIR="${SOURCE_DIR%/*}";
